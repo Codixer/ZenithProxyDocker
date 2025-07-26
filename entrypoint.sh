56f2d957e5b496da67a41351f7d8274a47b41ff0
Stefano <stefano@stefanocoding.me>
2025-07-26 13:11:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-cd /home/container/ZenithProxy
 
 # Print architecture for logs
 uname -m
 
 # Replace startup variables
 MODIFIED_STARTUP=$(eval echo "$STARTUP")
-echo ":/home/container/ZenithProxy$ $MODIFIED_STARTUP"
+echo ":/home/container$ $MODIFIED_STARTUP" # Adjusted output to reflect new WORKDIR
 
 # Run the server
 exec $MODIFIED_STARTUP
